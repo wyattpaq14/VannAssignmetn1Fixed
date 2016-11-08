01d24b97c6301153e20549b19d7c7ed4b661ec11
paqui <paqui@WYATT-LAPTOP>
2016-11-07 21:52:23 -0500

    Menus Class

---
+++ VannAssignment1Fixed/App_Code/menus.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace VannAssignment1Fixed.App_Code
+{
+    public class menus
+    {
+        public static List<MenuItem> getMenu()
+        {
+            List<MenuItem> items = new List<MenuItem>();
+            items.Add(new MenuItem("item1", "", "", "~/home"));
+            items.Add(new MenuItem("item2", "", "", "~/home"));
+            items.Add(new MenuItem("item3", "", "", "~/home"));
+            items.Add(new MenuItem("item4", "", "", "~/home"));
+            items.Add(new MenuItem("item5", "", "", "~/home"));
+            items.Add(new MenuItem("item6", "", "", "~/home"));
+            items.Add(new MenuItem("item7", "", "", "~/home"));
+
+
+            return items;
+        }
+    }
+}
